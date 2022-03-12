926d615b989287e781b2ce78d1c61da93bff487a
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-12 21:41:48 +0000

    My num5 c commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,13 +11,13 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 'a' alphabet <= 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet == 'q')
-continue:
+continue;
 
 else if (alphabet == 'e')
-continue:
+continue;
 
 putchar(alphabet);
 }
