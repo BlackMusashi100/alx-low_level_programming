f7c0c5a9409ddbeb776410e73d039bc9a6f5e4fa
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-12 22:02:06 +0000

    My num6 c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * 
+ * Return Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int x;
+
+for (x = 0 ; x < 10; x++)
+{
+printf("x", x);
+}
+
+printf("\n");
+
+return (0)
+
+}
