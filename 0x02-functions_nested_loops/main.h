a53e53b2873c6d287bd71c967751373ae2535c39
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-16 00:52:16 +0000

    My putchar commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,3 @@
-#include "0-putchar.c"
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success/correct 1.
- *on error, return an error
- */
-int _putchar(char c)
-
-{
-	return (write(1, &c, 1));
-}
 
 int _putchar(char c);
 void print_alphabet(void);
