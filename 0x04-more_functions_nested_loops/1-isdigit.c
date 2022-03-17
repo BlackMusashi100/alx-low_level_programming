132369862538eb64fd79a39a922602537108f6e7
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-17 21:26:45 +0000

    My isdigit commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isgit - if is a digit
+ * _isdigit - if is a digit
  *@ac: print int
  * Return: Always 0.
  */
 
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57);
+if (c >= 48 && c <= 57)
 {
 return (1);
 }
@@ -16,5 +16,4 @@ else
 {
 return (0);
 }
-
 }
