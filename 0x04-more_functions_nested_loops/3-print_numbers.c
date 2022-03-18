4ceff30dc8db59a6119bf40b67b29539acc2b905
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-18 13:59:56 +0000

    My c print commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
- * Return: Always 0.
- */
-void print_numbers(void)
+ * print_numbers(void)
 {
 int i;
 
 i = 0;
 while (i < 10)
 {
-_putchar (i);
+_putchar (i + '0');
 i++;
 }
 
