7dfb030fdf9d529fcddf83cb283f6659dad1b747
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-22 00:06:10 +0000

    My num3c pointer commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  *_strlen -length of a string
  *@s: A pointer to an int that will be changed/updated
  *
- *Return: void that means our answer is correct
+ *Return: void means our answer is correct
  */
 
 int _strlen(char *s)
@@ -13,8 +13,8 @@ int u;
 
 u = 0;
 while (s[u] != '\0')
-[
+{
 u++;
-]
+}
 return (0);
-]
+}
