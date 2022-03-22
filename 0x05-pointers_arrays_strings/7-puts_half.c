641ab062f0235420e2e191b13b2badf55e439fa5
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-22 02:05:18 +0000

    My num8c pointer commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,14 @@ void puts_half(char *str)
 int i, last;
 
 i = 0;
-while (str[1] != '\0')
+while (str[i] != '\0')
 {
 i++
 }
 
 last = (i + 1) / 2;
 
-for (i = last; str[1]; i++)
+for (i = last; str[i]; i++)
 {
 _putchar (str[i]);
 }
