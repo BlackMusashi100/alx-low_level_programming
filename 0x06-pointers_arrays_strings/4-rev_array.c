b1966a732976dd4b9e6d6e929b7cdbf7af6cc1c2
Shaban Haruna <shabanharuna100@gmail.com>
2022-03-24 13:05:15 +0000

    pointer4 commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *reverse_array - reverse the content of an array of integers
+ * reverse_array - reverse the content of an array of integers
  * @a: an array of integers
  * @n: number of elements of the array
  * Return: returns void
